69f9651334ed7b0be825b5eb681c1b8f4b1b63ce
Carlos Hughes <chughe88@kennesaw.edu>
2021-02-18 22:22:48 -0500

    added Material UI/ appbar

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/ToolBar";
+import { makeStyles } from "@material-ui/core/styles";
+import "./App.css";
+
+function App() {
+  const [joke, setJoke] = useState("");
+
+  function handleClick(e) {
+    e.preventDefault();
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.icndb.com/jokes/random/1?limitTo=[nerdy]", true); //api.icndb.com/jokes/random?exclude=[nerdy,explicit]
+
+    xhr.onload = function () {
+      if (this.status === 200) {
+        const response = JSON.parse(this.responseText);
+
+        let output = "";
+
+        if (response.type === "success") {
+          response.value.forEach(function (joke) {
+            output += `${joke.joke}`;
+          });
+        } else {
+          output = "Opps something went wrong";
+        }
+        const regex = /Chuck Norris/gi;
+        return setJoke(
+          output
+            .replace(regex, "Carlos Hughes")
+            .replace(/&quot;/g, '"')
+            .replace(/ChuckNorrisException/g, "CHughesExeption")
+            .replace(/Norris/g)
+        ); //.replace(/&quot;/g,'"')
+      }
+    };
+
+    xhr.send();
+  }
+
+  const useStyles = makeStyles((theme) => ({
+    appbar: { marginBottom: "10px" },
+    toolbar: {
+      // minHeight: 128,
+      // alignItems: 'flex-start',
+      // paddingTop: theme.spacing(1),
+      // paddingBottom: theme.spacing(2),
+      backgroundColor: "#FFDD99",
+    },
+  }));
+
+  const classes = useStyles();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AppBar className={classes.appbar}>
+          <ToolBar className={classes.toolbar}></ToolBar>
+        </AppBar>
+        <h1 style={{ fontSize: "50px" }}>Carlos J Hughes</h1>
+        <h1 style={{ fontSize: "30px", color: "#FFDD99" }}>Why should you work with Carlos?</h1>
+        <button className="button" onClick={handleClick}>
+          Click to find out
+        </button>
+        <p>{joke}</p>
+        <img
+          className="image"
+          src="https://phtbucket321.s3.amazonaws.com/81296033_10162863759680381_780491077755863040_n+(1).jpg"
+        />
+      </header>
+      <span>
+        <h3>link1</h3>
+        <h3>link2</h3>
+      </span>
+    </div>
+  );
+}
+
+export default App;
